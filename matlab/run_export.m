8504c027ed94327237bbe00b980edeee5c8f0edf
Matteo Rambaldi <106387960+MattRamb97@users.noreply.github.com>
2025-09-28 22:21:12 +0200

    Delete matlab directory

--- matlab/run_export.m
+++
@@ -1,8 +0,0 @@
-addpath(genpath('matlab')); addpath('deployment');
-load matlab/resRand.mat
-load matlab/resRandRGB.mat
-load matlab/resSAR.mat
-
-opts.outDir = "deployment/onnx";
-opts.modelNames = {'dense_rand','dense_randrgb','dense_sar'};
-Export_ONNX({resRand,resRandRGB,resSAR}, opts);
